d0e463b21d1ad0021c461af95610aa65e4a66049
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-05-11 12:03:26 +0800

    Update xyzt.c

--- xyzt.c
+++ xyzt.c
@@ -1,16 +1,14 @@
+let c = 299792458;
+const t1 = 0.1/c;
+const t2 = 0.2/c;
 
-#define c 299792458
-void integer(){
-    int t1 = 0.1/c;
-    int t2 = 0.2/c;
+const t3 = 0.3/c;
+const t4 = 0.4/c;
+const t5 = 0.5/c;
 
-    int t3 = 0.3/c;
-    int t4 = 0.4/c;
-    int t5 = 0.5/c;
+const t6 = 0.6/c;
+const t7 = 0.7/c;
 
-    int t6 = 0.6/c;
-    int t7 = 0.7/c;
-
-    int t8 = 0.8/c;
-    int t9 = 0.9/c;
-}
+const t8 = 0.8/c;
+const t9 = 0.9/c;
+ 
